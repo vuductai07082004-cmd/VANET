78ef1b9478c373246397ce258666f69a139b6f49
vuductai07082004-cmd <vuductai07082004@gmail.com>
2026-05-08 20:06:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ try:
 model = joblib.load('vanet_nbc_model.pkl')
 scaler = joblib.load('vanet_scaler.pkl')
 mean_data = joblib.load('vanet_means.pkl')
-        return model, scaler, mean_data
-    except Exception as e:
-        st.error(f"Lỗi tải file hệ thống: {e}")
-        return None, None, None
+return model, scaler, mean_data
+except Exception as e:
+st.error(f"Lỗi tải file hệ thống: {e}")
+return None, None, None
 
 model, scaler, mean_data = load_assets()
 
